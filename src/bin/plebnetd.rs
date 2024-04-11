0ccdda080e3f16c1088c919bb3eb5e4af70a6a11
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-04-11 14:13:48 -0400

    plebnetd:initial commit

---
+++ src/bin/plebnetd.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+// the download feature is enabled whenever a specific version is enabled, for example `25_1` or `24_0_1`
+//#[cfg(feature = "download")]
+//{
+  use bitcoincore_rpc::RpcApi;
+  let bitcoind = bitcoind::BitcoinD::from_downloaded().unwrap();
+  assert_eq!(0, bitcoind.client.get_blockchain_info().unwrap().blocks);
+//}
+
+}
